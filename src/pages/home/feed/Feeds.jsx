fcb5626b2dc8a5aa152725d8b13044455b48a855
Francis james <ibolokefrancisjames2@gmail.com>
2025-09-10 12:28:28 +0100

    add images for feed

--- src/pages/home/feed/Feeds.jsx
+++ src/pages/home/feed/Feeds.jsx
@@ -2,34 +2,45 @@ import React from 'react'
 import FeedSug from './FeedSug'
 import FeedsHeader from './FeedsHeader'
 import FeedsFoot from './FeedsFoot'
+import datas from  '../../../data/feeds.json'
+
 
 const Feeds = () => {
   return (
     <>
-       {/* feeds */}
-       <div className="feed shadow rounded-[10px] overflow-hidden p-4 space-y-2">
+    {/* feeds */}
+        {
+            datas && datas?.map(data => {
+                return (
+                    <div className="feed shadow rounded-[10px] overflow-hidden p-4 space-y-2">
             <FeedSug />
 
-            <FeedsHeader />
+                    <div className="mb-4">
+                        <FeedsHeader data={data}/>
 
-            <article className="writeUp">
-                <p className='text-[1rem]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro provident pariatur iste praesentium earum suscipit consequatur doloremque minima nostrum voluptatem.</p>
-            </article>{/* writeUp */}
+                        <article className="writeUp">
+                            <p className='text-[1rem]'>{data.writeUp}</p>
+                        </article>{/* writeUp */}
 
-            <div className="image space-y-1.5">
-                <img src="" alt="" className='border w-full h-[200px] border-black'/>
-                <div className='flex text-[0.8rem] opacity-50 justify-between'>
-                    <a className='hover:text-[#0A66C2] hover:underline hover:opacity-100'>👍👌 2,565</a>
-                    <div className='flex gap-2'>
-                        <a className='hover:text-[#0A66C2] hover:underline hover:opacity-100'>194 comments. </a>
-                        <a className='hover:text-[#0A66C2] hover:underline hover:opacity-100'> 256 repost</a>
+                        <div className="image space-y-1.5">
+                            <img src={data.displayUrl} alt="" className='border w-full h-[200px] border-black'/>
+                            <div className='flex text-[0.8rem] opacity-50 justify-between'>
+                                <a className='hover:text-[#0A66C2] hover:underline hover:opacity-100'>👍👌 2,565</a>
+                                <div className='flex gap-2'>
+                                    <a className='hover:text-[#0A66C2] hover:underline hover:opacity-100'>194 comments. </a>
+                                    <a className='hover:text-[#0A66C2] hover:underline hover:opacity-100'> 256 repost</a>
+                                </div>
+                            </div>
+                        </div>{/* Feed image */}
                     </div>
-                </div>
-            </div>{/* Feed image */}
-
             <FeedsFoot />
 
         </div>
+                )
+            })
+        }
+       
+       
     </>
   )
 }
--- src/pages/home/feed/FeedsHeader.jsx
+++ src/pages/home/feed/FeedsHeader.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const FeedsHeader = () => {
+const FeedsHeader = ({ data }) => {
   return (
     <>
          <div className="feed-header ">
                 <div className='flex justify-between gap-2'>
                     <section className='flex gap-2'>
-                        <img src="" alt="" className='w-[50px] h-[50px] rounded-full border border-black'/>
+                        <img src={data.profilePicUrl} alt="" className='w-[50px] h-[50px] rounded-full border border-black'/>
                         <div className="userTitle">
-                            <h3 className='text-[1rem] font-[500]'>Pascal BORNET</h3>
-                            <p className='text-[0.75rem]'>Award-winning AI & Automation Expert, 20+ years | Age... </p>
+                            <h3 className='text-[1rem] font-[500]'>{data.name}</h3>
+                            <p className='text-[0.75rem]'>{data.shortBio}</p>
                             <a className='text-[0.8rem] font-bold text-[#0A66C2] hover:underline'>View my blog</a>
                         </div>
                     </section>
